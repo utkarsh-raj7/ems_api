2c97592d149238c8934e7ee512674067ae19e8c2
Utkarsh Raj <utkarshraj@Utkarshs-MacBook-Pro.local>
2026-05-15 15:11:27 +0530

    clean

--- app/repository/database.py
+++ app/repository/database.py
@@ -1,10 +1,8 @@
- import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.common.config.config import settings
 
-encoded_password = urllib.parse.quote_plus(settings.db_password)
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{settings.db_user}:{encoded_password}@{settings.db_host}/{settings.db_name}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=settings.db_pool_size)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
